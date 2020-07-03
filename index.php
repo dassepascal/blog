e350da35b2fdefeee4c633956d07287b830e9267
dassepascal <pascaldasse@yahoo.fr>
2020-07-03 15:40:39 +0200

    ini

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>blog</title>
+<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <div class="container">
+    <div class="header">
+    <h1>Mon super blog !</h1>
+    </div>
+    
+    <a href="#">Retour à la liste des billets</a>
+    <div class="news">
+        <form method="POST" action="commentaires.php">
+            
+                <input type="text" name="titre" id="titre"><br/>
+              
+               <input type="text" name="contenu" id="contenu">
+        </form>  
+    </div>
+    </div>
+   
+</body>
+</html>
