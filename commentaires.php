9cd6c48baf6d65c35b771bdf600ae2c0d622cb0c
dassepascal <pascaldasse@yahoo.fr>
2020-07-03 16:52:01 +0200

    init

---
+++ commentaires.php
@@ -0,0 +1,21 @@
+<?php require('inc_connexion.php');?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta class="utf-8">
+        <title>commentaires</title>
+    </head>
+    <body>
+   
+    
+    <h1>Mon super blog !</h1>
+    <a href="index.php">Retour à la liste des billets</a>
+
+    </body>
+</html>
+
+
+
+
+
+
